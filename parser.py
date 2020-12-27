b2ed615d7258fb7454c92a804bc51f3525e187a7
Alex <airman13k@gmail.com>
2020-12-28 02:21:25 +0300

    built .exe

--- parser.py
+++ parser.py
@@ -1,11 +1,11 @@
-from bs4 import BeautifulSoup
 import requests
 import json
 from lxml import html
 import pickle
 from tkinter import *
 from tkinter import ttk
-import os.path
+import os
+import numpy as np
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
@@ -13,46 +13,57 @@ import httplib2
 import apiclient.discovery
 from oauth2client.service_account import ServiceAccountCredentials
 import statistics
+import pygsheets
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+CREDENTIALS_FILE = 'D:\\Useful misc\\мплинкпарсер\\mplink_to_google_sheets\\match-results-19f739c32961.json'
 
 class ui:
-    link = 'https://osu.ppy.sh/community/matches/72098518'
-    spreadsheetId = '1Kst83QCbmRISmUt0zDO7938kZ9SdUczlpGlVh_qIVRU'
-    #spreadsheetId = '11fn6U7RhMPTD69EgZTgwvU_JSEOZt8QINd9mJ8Q8Y9s'
-    sheetname = 'Week 3(2)'
+    link = 'https://osu.ppy.sh/community/matches/71542504'
+    #spreadsheetId = '1Kst83QCbmRISmUt0zDO7938kZ9SdUczlpGlVh_qIVRU'
+    spreadsheetId = '11fn6U7RhMPTD69EgZTgwvU_JSEOZt8QINd9mJ8Q8Y9s'
+    sheetname = 'Week 2(1)'
     startColumnIndex = 7
     endColumnIndex = 67
-    startRowIndex = 3
+    startRowIndex = 2
     endRowIndex = 18
-    numberOfPeople = 0
-    incr = {}
+    names = []
     diff = {'Easy' : 600000, 'Medium' : 500000, 'Hard' : 400000, 'Markrum' : 150000}
-
     def __init__(self, root):
         root.title("Булщит в шит")
         root.geometry("500x100")
-        mainframe = ttk.Frame(root, padding = "3 3 12 12")
-        mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+        mainframe = ttk.Frame(root)
+        mainframe.pack(fill=BOTH, expand=True)
 
-        root.columnconfigure(0, weight=1)
-        root.rowconfigure(0, weight=1)
-
-        sheet_id = ttk.Entry(mainframe, width = 50, textvariable = self.spreadsheetId)
-        sheet_id.grid(column = 1, row = 1)
+        frame_top = Frame(mainframe)
+        frame_top.pack()
+        ttk.Label(frame_top, text="Айди дока: ").pack(side=LEFT)
+        sheet_id = ttk.Entry(frame_top, width=50)
+        sheet_id.pack(side=LEFT)
         sheet_id.insert(0, self.spreadsheetId)
-        ttk.Label(mainframe, text="Айди дока: ").grid(column = 0, row = 1)
 
-        ttk.Label(mainframe, text = "Название листа: ").grid(column=0, row=3)
-        sheetname_entry = ttk.Entry(mainframe, width = 20, textvariable = self.sheetname)
-        sheetname_entry.grid(column = 1, row = 3, sticky=(W, E))
-        sheetname_entry.insert(0, 'Week ()')
+        frame_mid = Frame(mainframe)
+        frame_mid.pack()
+        ttk.Label(frame_mid, text = "Название листа: Week ").pack(side=LEFT)
+        sheetname_entry1 = ttk.Entry(frame_mid, width = 2, textvariable = self.sheetname)
+        sheetname_entry1.pack(side=LEFT)
+        ttk.Label(frame_mid, text = "(").pack(side=LEFT)
+        sheetname_entry2 = ttk.Entry(frame_mid, width = 2, textvariable = self.sheetname)
+        sheetname_entry2.pack(side=LEFT)
+        ttk.Label(frame_mid, text = ")").pack(side=LEFT)
+        
+        frame_third = Frame(mainframe)
+        frame_third.pack()
+        ttk.Label(frame_third, text="MP link: ").pack(side=LEFT)
+        link_space = ttk.Entry(frame_third, width=50, textvariable = self.link)
+        link_space.pack(side=LEFT)
 
-        link_space = ttk.Entry(mainframe, width = 50, textvariable = self.link)
-        link_space.grid(column = 1, row = 5, sticky=(W, E))
-        ttk.Label(mainframe, text="MP link: ").grid(column=0, row = 5)
+        frame_bot = Frame(mainframe)
+        frame_bot.pack()
+        ttk.Button(frame_bot, text="Отправить результаты", command = lambda: self.get_shit_to_sheet(link_space, sheetname_entry1, sheetname_entry2, sheet_id)).pack(side=LEFT)
+        self.status = ttk.Label(frame_bot, text='Ready')
+        self.status.pack(side=LEFT)
 
-        ttk.Button(mainframe, text="Отправить результаты", command = lambda: self.get_shit_to_sheet(link_space, sheetname_entry, sheet_id)).grid(column = 0, row = 6, sticky =W)
-        root.bind("<Return>", lambda: self.get_shit_to_sheet(link_space, sheetname_entry, sheet_id))
+        root.bind("<Return>", lambda: self.get_shit_to_sheet(link_space, sheetname_entry1, sheetname_entry2, sheet_id))
         link_space.focus()
 
     def get_username_from_id(self, user_id):
@@ -64,23 +75,23 @@ class ui:
         return data["username"]
 
     def parse_the_link(self, link):
+        numberOfPeople = 0
         print('started parsing')
-        global numberOfPeople, endColumnIndex, endRowIndex
+        self.status.config(text = 'Parsing the mp link')
         page = requests.get(link)
         tree = html.fromstring(page.content)
         results = tree.xpath("//script[@id='json-events']/text()")
         data = json.loads(str(results[0]))
-        lobby_dict = {}
         dict_of_scores = {}
         counter = 0
         mappool_size = 0
-        rgb_string = '0 0 0'
         for item in data["events"]:
             if "game" in item.keys():
                 #print(json.dumps(item,indent=4))
                 for score in item["game"]["scores"]:
                     name = score["user_id"]
                     if name not in dict_of_scores.keys():
+                        numberOfPeople+=1
                         dict_of_scores[name] = [[], [], []]
                         for i in range(counter):
                             dict_of_scores[name][0].append(0)
@@ -89,35 +100,36 @@ class ui:
                             
                     dict_of_scores[name][0].append(score["score"])
                     dict_of_scores[name][1].append('%.2f'%(score["accuracy"]*100))
-                    rgb_string = '204 204 204'
-                    if 'HD' in score['mods']:
-                        rgb_string = "255 229 153"
-                    if 'HR' in score['mods']:
-                        rgb_string = '234 153 153'
+
                     if 'DT' in score['mods']:
-                        rgb_string = '180 167 214'
-                    dict_of_scores[name][2].append(rgb_string.split())
+                        dict_of_scores[name][2].append('DT')
+                    elif 'HR' in score['mods']:
+                        dict_of_scores[name][2].append('HR')
+                    elif 'HD' in score['mods']:
+                        dict_of_scores[name][2].append('HD')
+                    else:
+                        dict_of_scores[name][2].append('NM')
                 counter+=1
+
         temp = []
-        print(dict_of_scores)
+        
         for score in dict_of_scores.values():
             temp.append(len(score[0]))
+
         self.mappool_size = int(statistics.median(temp))
         print('mappool size = ' + str(self.mappool_size))
 
-        list_of_ids = list(dict_of_scores.keys())
-        names = []
-        self.numberOfPeople = len(list_of_ids)
-
-        self.endColumnIndex = self.startColumnIndex + self.numberOfPeople*2
+        self.endColumnIndex = self.startColumnIndex + numberOfPeople*2 - 1
+        print(self.endColumnIndex)
         counter = 0
-        for scores in dict_of_scores.values():
-            print(scores[0])
+
+        for scores in dict_of_scores.values(): #will readjust if someone had played the maps after the end of the lobby
             if len(scores[0]) < self.mappool_size:
                 for i in range(len(scores[0]), self.mappool_size):
                     scores[0].append(0)
                     scores[1].append(0)
-                    scores[2].append(0, 0, 0)
+                    scores[2].append(0)
+
             if len(scores[0]) > self.mappool_size:
                 for i in range(self.mappool_size):
                     if scores[0][i] == 0:
@@ -126,130 +138,84 @@ class ui:
                         scores[2][i] = scores[2].pop(self.mappool_size)
                         
 
-        self.endRowIndex = self.startRowIndex + self.mappool_size +  1
+        self.endRowIndex = self.startRowIndex + self.mappool_size + 1
+        print('endRowIndex = ' + str(self.endRowIndex))
 
-        for id in list_of_ids:
-            names.append(self.get_username_from_id(id))
-        
-        for i in range(len(list_of_ids)):
-            dict_of_scores[names[i]] = dict_of_scores.pop(list_of_ids[i])
+        for id in list(dict_of_scores.keys()):
+            self.names.append(self.get_username_from_id(id))
+
         print('done with parsing')
+        self.status.config(text = 'Finished parsing the mp link')
         return dict_of_scores
 
-    def fill_the_incr(self):
-        for i in range(71, 71 + self.numberOfPeople*2 +1):
-            if i < 89:
-                self.incr[chr(i)] = chr(i + 2)
-            else:
-                if i < 91:
-                    self.incr[chr(i)] = "A" + chr(i-64)
-                else:
-                    ch = "A" + chr(i-66)
-                    self.incr[ch] = "A" + chr(i-64)
-        print(self.incr)
-
-    def get_shit_to_sheet(self, link_space, sheetname_entry, sheet_id):
+    def get_shit_to_sheet(self, link_space, sheetname_entry1, sheetname_entry2, sheet_id):
         self.link = link_space.get()
-        self.sheetname = sheetname_entry.get()
+        self.sheetname = 'Week {}({})'.format(sheetname_entry1.get(), sheetname_entry2.get())
         if sheet_id.get() != None:
             self.spreadsheetId = sheet_id.get()
-        results = self.parse_the_link(self.link)
-        self.fill_the_incr()
-        CREDENTIALS_FILE = 'D:/Useful misc/мплинкпарсер/mplink_to_google_sheets/match-results-19f739c32961.json'
-        credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE,
-        ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive'])
-        httpAuth = credentials.authorize(httplib2.Http()) # Авторизуемся в системе
-        service = apiclient.discovery.build('sheets', 'v4', http = httpAuth)
 
-        service = build('sheets', 'v4', credentials=credentials)
-        spreadsheet = service.spreadsheets().get(spreadsheetId = self.spreadsheetId).execute()
-        difficulty = service.spreadsheets().values().get(spreadsheetId = self.spreadsheetId, 
-        range = self.sheetname + "!C23", valueRenderOption='UNFORMATTED_VALUE').execute()['values'][0][0]
+        results = self.parse_the_link(self.link)
+
+        self.status.config(text = 'Authorizing gsheets api') 
+        gc = pygsheets.authorize(service_file=CREDENTIALS_FILE)
+        sprdsheet = gc.open_by_key(self.spreadsheetId)
+        sheet = sprdsheet.worksheet_by_title(self.sheetname)
+
+        print(self.spreadsheetId)
+        print(self.sheetname)
+
+        difficulty = sheet.cell('C23').value_unformatted
         print(difficulty)
-        divider = self.diff[difficulty]
+        self.status.config(text = 'Prepping data for the api calls')
+        data_dump = []
+        color_ref = []
+        i = 0
+        #prepping data dumps for api calls
         for scores in results.values():
-            scores[0].append('%.2f'%(sum(scores[0])/self.mappool_size/divider))
-            print(scores[0][-1])
-        print("end row index " + str(self.endRowIndex))
-        self.columnToWrite = "G"
-        print('starting to fill the sheet')
-        for k in results.items():
-            batch_update_spreadsheet_request_body = {
-                "valueInputOption": "USER_ENTERED",
-                "data" : 
-                [
-                    {
-                        "range" : self.sheetname + "!" + self.columnToWrite +"3:AL25",
-                        "majorDimension" : "COLUMNS",
-                        "values" : 
-                        [
-                            k[1][0], k[1][1],
-                            {
-                                'red' : int(k[1][2][0]),
-                                'green' : int(k[1][2][1]),
-                                'blue' : int(k[1][2][2])
-                            }
-                        ]
-                    },
-                    {
-                        "range" : self.sheetname + "!"+ self.columnToWrite +"2",
-                        "majorDimension" : "rows",
-                        "values" : 
-                        [
-                            [k[0]]
-                        ]
-                    },
-                    {
-                        "range" : self.sheetname + "!F" + str(self.endRowIndex-1),
-                        "majorDimension" : "rows",
-                        "values" : 
-                        [
-                            ['Match cost: ']
-                        ]
-                    }
-                ]
-            }
-            res = service.spreadsheets().values().batchUpdate(spreadsheetId = self.spreadsheetId,
-            body = batch_update_spreadsheet_request_body).execute()
-            mod_color = service.spreadsheets().batchUpdate(spreadsheetid = self.spreadsheetId,
-            body = {
-                "requests":     
-                [
-                    {
-                        "updateCells": 
-                        {
-                            "range": 
-                            {
-                                "sheetId": sheetId,
-                                "startRowIndex": 0,
-                                "endRowIndex": 1,
-                                "startColumnIndex": 0,
-                                "endColumnIndex": 1
-                            },
-                            "rows": 
-                            [
-                                {
-                                    "values": 
-                                    [
-                                        {
-                                            "userEnteredFormat": 
-                                            {
-                                                "backgroundColor":
-                                                {
-                                                    "red": 1
-                                                }
-                                            }
-                                        }
-                                    ]
-                                }
-                            ],
-                            "fields": "userEnteredFormat.backgroundColor"
-                        }
-                    }
-                ]
-            })
-            self.columnToWrite = self.incr[self.columnToWrite]
-            print('done with ' + k[0])
+            scores[0].append('%.2f'%(sum(scores[0])/self.mappool_size/self.diff[difficulty]))
+            scores[0].insert(0, self.names[i])
+            data_dump.append(scores[0])
+            scores[1].insert(0, None)
+            data_dump.append(scores[1])
+            color_ref.append(scores[2])
+            color_ref.append(scores[2])
+            i+=1
+
+        sheet.update_value((self.endRowIndex, 6), 'Match cost: ')
+
+        values = list(results.values())
+        
+        data_range = pygsheets.datarange.DataRange(start=(self.startRowIndex, self.startColumnIndex), end=(self.endRowIndex, self.endColumnIndex), worksheet=sheet)
+        color_range = pygsheets.datarange.DataRange(start=(self.startRowIndex + 1, self.startColumnIndex), end=(self.endRowIndex - 1, self.endColumnIndex), worksheet=sheet)
+        
+        le_epic_color_range = pygsheets.datarange.DataRange(start = (self.startRowIndex+50, self.startColumnIndex), end = (self.endRowIndex+50, self.endColumnIndex), worksheet=sheet)
+        self.status.config(text = 'Inputting score data')
+
+        sheet.update_values(crange = data_range.range, values = data_dump, majordim = 'COLUMNS', parse=False)
+
+        sheet.update_values(crange = le_epic_color_range.range, values = color_ref, majordim = 'COLUMNS', parse=False)
+
+        self.status.config(text = 'Epic color programming')
+        
+        #adding rules for coloring cells
+        sheet.add_conditional_formatting(start = (self.startRowIndex + 1, self.startColumnIndex), end = (self.endRowIndex-1, self.endColumnIndex), condition_type='CUSTOM_FORMULA',
+         format={'backgroundColor' : {'red' : 234/255, 'green' : 153/255, 'blue' : 153/255, 'alpha' : 1}}, condition_values=['=(G{}=\"HR\")'.format(self.startRowIndex+50)])
+
+        sheet.add_conditional_formatting(start = (self.startRowIndex + 1, self.startColumnIndex), end = (self.endRowIndex-1, self.endColumnIndex), condition_type='CUSTOM_FORMULA',
+         format={'backgroundColor' : {'red' : 180/255, 'green' : 167/255, 'blue' : 214/255, 'alpha' : 1}}, condition_values=['=(G{}=\"DT\")'.format(self.startRowIndex+50)])
+
+        sheet.add_conditional_formatting(start = (self.startRowIndex + 1, self.startColumnIndex), end = (self.endRowIndex-1, self.endColumnIndex), condition_type='CUSTOM_FORMULA',
+         format={'backgroundColor' : {'red' : 255/255, 'green' : 229/255, 'blue' : 153/255, 'alpha' : 1}}, condition_values=['=(G{}=\"HD\")'.format(self.startRowIndex+50)])
+
+        sheet.add_conditional_formatting(start = (self.startRowIndex + 1, self.startColumnIndex), end = (self.endRowIndex-1, self.endColumnIndex), condition_type='CUSTOM_FORMULA',
+         format={'backgroundColor' : {'red' : 250/255, 'green' : 238/255, 'blue' : 243/255, 'alpha' : 1}}, condition_values=['=(G{}=\"NM\")'.format(self.startRowIndex+50)])
+
+        sheet.add_conditional_formatting(start = (self.startRowIndex + 1, self.startColumnIndex), end = (self.endRowIndex-1, self.endColumnIndex), condition_type='CUSTOM_FORMULA', 
+          format={'backgroundColor' : {'red' : 75/255, 'green' : 75/255, 'blue' : 75/255, 'alpha' : 1}}, condition_values=['=(G{}=0)'.format(self.startRowIndex+50)])
+
+        self.status.config(text = 'Finished updating {}'.format(self.sheetname))
+        self.names.clear()
+        print('finished updating {}'.format(self.sheetname))
 
 root = Tk()
 ui(root)
